dd562757fb5f657b400d28440c7b46391eb51462
Anatoli Marian <bmwm5f15.tm@gmail.com>
2024-01-22 22:46:39 +0200

    Реализация фильтрации массива

--- Program.cs
+++ Program.cs
@@ -15,7 +15,32 @@ static void Main()
         PrintArray(resultArray);
     }
 
+static string[] FilterArray(string[] inputArray)
+    {
+        
+        int count = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
 
+        string[] resultArray = new string[count];
+
+        int index = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                resultArray[index] = item;
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
 
 
 
