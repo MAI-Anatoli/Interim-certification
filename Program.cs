5eec96e31292cd43e00e75b8954efa7ff539cb5b
Anatoli Marian <bmwm5f15.tm@gmail.com>
2024-01-22 22:40:02 +0200

    Реализация ввода элементов массива

--- Program.cs
+++ Program.cs
@@ -3,6 +3,17 @@
 class Program
 {
 
+static void Main()
+    {
+        Console.Write("Введите элементы массива через пробел: ");
+        string input = Console.ReadLine();
+
+        string[] inputArray = input.Split(' ');
+
+        string[] resultArray = FilterArray(inputArray);
+
+        PrintArray(resultArray);
+    }
 
 
 
